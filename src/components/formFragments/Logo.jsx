af097db0ef8acac059c89cd76d1f1088876f804f
Omar Labana <omarlabana@outlook.com>
2021-11-23 18:57:38 +0200

    Add simple logo

---
+++ src/components/formFragments/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <h1 className="flex items-baseline justify-center my-1">
+      <span className="mx-1 block w-2 h-8 bg-green-400 rounded-md"></span>
+      <span className="mx-1 block w-2 h-12 bg-green-700 rounded-md"></span>
+      <span className="text-5xl font-semibold text-gray-700">trendo</span>
+    </h1>
+  );
+};
+
+export default Logo;
